2cd7ec9c1183109e5d7c63d212d798b588c20c93
Laura Groetzinger <laura.groetzinger@gmail.com>
2020-04-12 17:27:45 -0400

    Added some checkboxes and stuff on the left side, updated the favicon and the webpack command in the readme. You are cute

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
-
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -9,10 +9,17 @@ import TextField from '@material-ui/core/TextField';
 
 
 import HeaderLogo from './saltcolorlab.svg';
+import { FormLabel, FormGroup, FormControl, FormControlLabel, Checkbox } from "@material-ui/core";
 
-const ContainerShell = styled.ul`
+const ContainerShell = styled.div`
   width: 1120px;
   margin: auto;
+`;
+
+const ColorListing = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #000;
 `;
 
 export default function MainComponent() {
@@ -22,12 +29,11 @@ export default function MainComponent() {
         <Grid item xs={12}>
           <Header />
         </Grid>
-
-        <Grid xs={3}>
-          Left Column
+        <Grid xs={2}>
+          <SideBar />
         </Grid>
 
-        <Grid xs={9}>
+        <Grid xs={10}>
           <ColorInventory />
         </Grid>
 
@@ -39,20 +45,11 @@ export default function MainComponent() {
   );
 }
 
-function ColorInventory() {
+function SideBar() {
   return (
-    <Box display="flex">
-      <Box p={1} flexGrow={1}>
-        <TextField
-          placeholder="Search color..."
-          variant="outlined"
-          size="small"
-        />
-      </Box>
-
-      <Box p={1}>
-        Sort by:
-
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Browse Colors</FormLabel>
+      <FormGroup>
         <Select
           size="small"
           variant="outlined"
@@ -60,11 +57,53 @@ function ColorInventory() {
           displayEmpty
           style={{ height: '40px' }}
         >
-          <MenuItem value="">Most Permanent</MenuItem>
-          <MenuItem value="least-expensive">Least Expensive</MenuItem>
+          <MenuItem value="">All Colors</MenuItem>
+          <MenuItem value="blues">Blues</MenuItem>
+          <MenuItem value="blues">Reds</MenuItem>
+          <MenuItem value="blues">Yellows</MenuItem>
         </Select>
+      </FormGroup>
+      <FormLabel component="legend">Lightfastness</FormLabel>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox />} label="Excellent (I)" />
+        <FormControlLabel control={<Checkbox />} label="Very Good (II)" />
+        <FormControlLabel control={<Checkbox />} label="Fair (III)" />
+        <FormControlLabel control={<Checkbox />} label="Poor (IV)" />
+        <FormControlLabel control={<Checkbox />} label="Very Poor (V)" />
+      </FormGroup>
+    </FormControl>
+  );
+}
+
+function ColorInventory() {
+  return (
+    <div>
+        <Box display="flex">
+        <Box p={1} flexGrow={1}>
+          <TextField
+            placeholder="Search color..."
+            variant="outlined"
+            size="small"
+          />
+        </Box>
+
+        <Box p={1}>
+          Sort by:
+
+          <Select
+            size="small"
+            variant="outlined"
+            value=""
+            displayEmpty
+            style={{ height: '40px' }}
+          >
+            <MenuItem value="">Most Permanent</MenuItem>
+            <MenuItem value="least-expensive">Least Expensive</MenuItem>
+          </Select>
+        </Box>
       </Box>
-    </Box>
+      <ColorListing />
+    </div>
   );
 }
 
